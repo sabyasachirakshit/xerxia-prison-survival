cdc16619b7466faf50b4a0451c6e2d92620aaf5d
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2024-08-06 10:02:44 +0530

    project initialised

---
+++ src/components/LoadProfile.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function LoadProfile() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <h1>LOAD PROFILE</h1>
+      <div className="profiles-list">
+        
+      </div>
+    </div>
+  );
+}
+
+export default LoadProfile;
